b5212420576e811d67a6099b2e9fea2ec33cefa5
Dogu Can Elci <elci15@itu.edu.tr>
2024-02-12 02:48:18 +0300

    121

---
+++ scripts/sp_CreateSQLServerlessView_gold.sql
@@ -0,0 +1,12 @@
+USE gold_db 
+GO 
+CREATE OR ALTER PROC CreateSQLServerlessView_gold @ViewName nvarchar(100) AS BEGIN 
+DECLARE @statement VARCHAR(MAX) 
+
+
+SET @statement = N'CREATE OR ALTER VIEW ' + @ViewName + " AS SELECT * FROM 
+OPENROWSET( BULK ''https://dogudatalakegen2.dfs.core.windows.net/gold/Sales/' + @ViewName + '/'' ,FORMTAT= ''DELTA'' ) as [result] "
+EXEC (@statement) 
+END 
+GO 
+
